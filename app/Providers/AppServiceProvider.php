4e00a74c31bc3a4a23dab051f0034b65e8b11533
fiyinfholuwa <fiyinfholuwa@gmail.com>
2023-03-02 08:16:16 +0100

    windsorAPP updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,6 @@ class AppServiceProvider extends ServiceProvider
     {
         // Using view composer to set following variables globally
         view()->composer('*',function($view) {
-           
            $view->with('count_payment', PaymentConfirm::where('verify_status', '=', 'Pending')->get());
            $view->with('payment_status', PaymentConfirm::where('verify_status', '=', 'Pending')->orderBy('id', 'Desc')->offset(0)->limit(3)->get());
         });
